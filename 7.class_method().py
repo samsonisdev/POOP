ace55431ada240dfb2b4e1f9bd27ac7d0a6ea507
Samson Rizz <samsonrizzy6@gmail.com>
2024-07-18 13:31:26 +0500

    Add files via upload

---
+++ 7.class_method().py
@@ -0,0 +1,39 @@
+# Class Method: is bound to the class and receives the class as an implicit first argument
+# Note - static methods can't access or modify class state and generally for utility
+
+# So, in the code below, we have two attributes with the same name 'name' and self.name
+# when we change the name, the name = 'anonymous' doesn't really change
+# cuz in the method, changeName(), the method creates another attribute self.name
+# which doesn't affect the class attribute 'name'
+
+class Person:
+
+    name = "anonymous" # class attribute
+
+    def changeName(self, name):
+        self.name = name
+
+    # So, to change the class attribute 'name', we access the class attribute
+    # For e.g:
+    def change(self, name):
+        Person.name = name
+
+    # Another way to do the same thing
+    # Here, __class__ is actually representing the class name 'Person'
+    # So, Person.name OR __class__.name, it's the same thing
+    def changing(self, name):
+        self.__class__.name = name
+
+    # But another way to do it with class method is creating the class method
+    # which help us access class attribute directly
+    @classmethod
+    def changeThisName(cls, name):
+        cls.name = name
+
+
+
+per = Person()
+per.changeThisName('Samson')
+print(per.name)
+print(Person.name)
+
---
+++ 8.polymorphism.py
@@ -0,0 +1,43 @@
+# Polymorphism: Operator Overloading
+# Poly means many, morph means forms
+# When the same operator is allowed to have different meaning according to the context
+# For e.g:
+
+# Here + means
+# print(1 + 2) # adding two numbers
+# print("Samson" + "Rizz") # combining to strings
+# print([1, 2, 3] + [4, 5, 6]) # merging two lists
+
+# So, the + has different meanings according to the context/data types
+# This is polymorphism when one thing has many forms according to the context
+
+# Functions like __add__, __init__, __sub__, etc. are called Dunder Functions
+
+class Complex:
+
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showComplex(self):
+        print(self.real, "i +", self.img, "j")
+
+    def __add__(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal, newImg)
+
+    def __sub__(self, num2):
+        newReal = self.real - num2.real
+        newImg = self.img - num2.img
+        return Complex(newReal, newImg)
+
+num1 = Complex(4, 6)
+num1.showComplex()
+
+num2 = Complex(4, 4)
+num2.showComplex()
+
+ans = num1 - num2
+print("---------")
+ans.showComplex()
